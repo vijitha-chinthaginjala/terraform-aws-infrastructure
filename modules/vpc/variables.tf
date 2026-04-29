b8b67d7655afdc859746f91f9a4370ff90725d8a
vijitha-chinthaginjala <chinthaginjalavijitha@gmail.com>
2026-04-29 11:48:44 -0500

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, production)"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
